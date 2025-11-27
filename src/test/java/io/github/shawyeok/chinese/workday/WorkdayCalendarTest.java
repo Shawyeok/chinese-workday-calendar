0d2df6afcd37701e0b2221b4b82e41e8db0a5117
Yike Xiao <kmter@live.com>
2025-11-27 15:31:07 +0800

    Generate workday data for the year 2026

--- src/test/java/io/github/shawyeok/chinese/workday/WorkdayCalendarTest.java
+++ src/test/java/io/github/shawyeok/chinese/workday/WorkdayCalendarTest.java
@@ -14,7 +14,7 @@ class WorkdayCalendarTest {
     @CsvFileSource(resources = {
             "2007.csv", "2008.csv", "2009.csv", "2010.csv",
             "2011.csv", "2012.csv", "2013.csv", "2014.csv", "2015.csv", "2016.csv", "2017.csv", "2018.csv", "2019.csv",
-            "2020.csv", "2021.csv", "2022.csv", "2023.csv", "2024.csv", "2025.csv"})
+            "2020.csv", "2021.csv", "2022.csv", "2023.csv", "2024.csv", "2025.csv", "2026.csv"})
     void isWorkday(String date, int expected) {
         if (expected == 0) {
             assertFalse(calendar.isWorkday(date));
